d427acfec134ca5b6833c5f471c13cb40ef73728
orlan <orlando.alessio1998@gmail.com>
2022-09-05 21:57:34 +0200

    Added html interface, removed streamlit implementation

--- main.py
+++ main.py
@@ -1,25 +1,141 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 import text_extraction
-import text_playback
+import asyncio
+import os
+import eel
+import shutil
+import subprocess
+from gtts import gTTS
+import os
+import asyncio
+import pyaudio
+import wave
+import time
+from pynput import keyboard
+
+ebook_path = os.path.join(".", "ebook")
+books = os.listdir(ebook_path)
+sample = os.path.join(ebook_path, books[0])
+
+paused = False  # global to track if the audio is paused
+objpath = os.path.join(".", "out")
+shutil.rmtree(objpath)
+os.makedirs(objpath)
+
+book_loaded = None
 
 
-book_path = "./Shelf/fleming-casino-royale.epub"
-book2_path = "./Shelf/various-childrens-book-of-christmas-stories.epub"
-book_sw_path = "./Shelf/Star Wars - Aftermath - Debito di Vita (Chuck Wendig).epub"
+async def load_next(text, language, slow, path):
+	next_obj = gTTS(text=text, lang=language, slow=slow)
+	next_obj.save(path + '.mp3')
+	subprocess.call(['./ffmpeg/bin/ffmpeg.exe', '-y', '-i', path + '.mp3', path + '.wav'])
 
-title, text = text_extraction.extract_text(book_path)
-clean = text_extraction.clean_text(text)
-print(title)
-print(text)
-print(clean)
 
-for chapter in clean:
-    sentences = clean[chapter]
-    text_playback.play_sentence(chapter)
-    for sentence in sentences:
-        text_playback.play_sentence(sentence)
+def play(p, wf):
+	# define callback
+	def callback(in_data, frame_count, time_info, status):
+		data = wf.readframes(frame_count)
+		return (data, pyaudio.paContinue)
+
+	# open stream using callback
+	stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+	                channels=wf.getnchannels(),
+	                rate=wf.getframerate(),
+	                output=True,
+	                stream_callback=callback)
+
+	def on_press(key):
+		global paused
+		print(key)
+		if key == keyboard.Key.space:
+			if stream.is_stopped():  # time to play audio
+				print('play pressed')
+				stream.start_stream()
+				paused = False
+				return False
+			elif stream.is_active():  # time to pause audio
+				print('pause pressed')
+				stream.stop_stream()
+				paused = True
+				return False
+		return False
+
+	# start the stream
+	stream.start_stream()
+
+	while stream.is_active() or paused:
+		with keyboard.Listener(on_press=on_press) as listener:
+			listener.join()
+
+		time.sleep(0.05)
+
+	# stop stream
+	stream.stop_stream()
+	stream.close()
+	wf.close()
+
+
+async def play_sentences(book, language="en", slow=False):
+	# instantiate PyAudio
+	p = pyaudio.PyAudio()
+
+	current_file_path = os.path.join(objpath, "current")
+	next_file_path = os.path.join(objpath, "next")
+	chapter_file_path = os.path.join(objpath, "chapter")
+
+	for chapter in book:
+
+		chapter_obj = gTTS(text=chapter, lang=language, slow=slow)
+		chapter_obj.save(chapter_file_path + ".mp3")
+		subprocess.call(['./ffmpeg/bin/ffmpeg.exe', '-y', '-i', chapter_file_path + '.mp3', chapter_file_path + '.wav'])
+		wf = wave.open(chapter_file_path + ".wav", 'rb')
+		play(p, wf)
+
+		sentences = book[chapter]
+		for index, items in enumerate(sentences):
+
+			if not os.path.exists(next_file_path + ".wav"):
+				current_obj = gTTS(text=sentences[index], lang=language, slow=slow)
+				current_obj.save(current_file_path + ".mp3")
+				subprocess.call(
+					['./ffmpeg/bin/ffmpeg.exe', '-y', '-i', current_file_path + '.mp3', current_file_path + '.wav'])
+			else:
+				os.rename(next_file_path + ".wav", current_file_path + ".wav")
+
+			'''
+			if index + 1 < len(sentences):
+				asyncio.create_task(
+					load_next(sentences[index + 1], language=language, slow=slow, path=next_file_path))
+			'''
+
+			# you audio here
+			wf = wave.open(current_file_path + ".wav", 'rb')
+
+			eel.update_html(book, chapter, index)
+			play(p, wf)
+
+	# close PyAudio
+	p.terminate()
+
+	return
+
+
+@eel.expose
+def load(book):
+	book = sample
+	global book_loaded
+	title, text = text_extraction.extract_text(book)
+	book_loaded = text_extraction.clean_text(text)
+	return book_loaded
+
+
+@eel.expose
+def read():
+	if book_loaded is not None:
+		asyncio.run(play_sentences(book_loaded))
+	return
+
+
+if __name__ == '__main__':
+	# name of folder where the html, css, js, image files are located
+	eel.init('templates')
+	eel.start('index.html', mode='chrome', host='localhost', port=8000)
--- text_extraction.py
+++ text_extraction.py
@@ -1,66 +1,44 @@
-import pdfplumber
-
-'''
-with pdfplumber.open(book_path) as pdf:
-    first_page = pdf.pages[1]
-    print(first_page.extract_text())
-'''
 import ebooklib
 from ebooklib import epub
+import nltk
+from bs4 import BeautifulSoup
 
 
 def extract_text(book_path):
-    book = epub.read_epub(book_path)
-    items = list(book.get_items())
-    chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
-    texts = {}
-    for c in chapters:
-        texts[c.get_name()] = chapter_to_str(c)
-    to_keep = []
-    keep = False
-    for chapter in texts:
-        if keep == True:
-            to_keep.append(chapter)
-        if "Copyright" in texts[chapter]:
-            keep = True
+	book = epub.read_epub(book_path)
+	items = list(book.get_items())
+	chapters = list(book.get_items_of_type(ebooklib.ITEM_DOCUMENT))
+	texts = {}
+	for c in chapters:
+		texts[c.get_name()] = chapter_to_str(c)
+	to_keep = []
+	keep = False
+	for chapter in texts:
+		if keep == True:
+			to_keep.append(chapter)
+		if "Copyright" in texts[chapter]:
+			keep = True
 
-    clean_text = {}
-    i = 1
-    for chapter in to_keep:
-        clean_text["Chapter " + str(i)] = texts[chapter]
-        i += 1
-    return book.title,clean_text
+	clean_text = {}
+	i = 1
+	for chapter in to_keep:
+		clean_text["Chapter " + str(i)] = texts[chapter]
+		i += 1
+	return book.title, clean_text
 
 
-from bs4 import BeautifulSoup
 def chapter_to_str(chapter):
-    soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
-    text = [para.get_text() for para in soup.find_all('p')]
-    #p = soup.find_all('p')
-    return ''.join(text)
-
-
-import nltk
-nltk.download('punkt')
+	soup = BeautifulSoup(chapter.get_body_content(), 'html.parser')
+	text = [para.get_text() for para in soup.find_all('p')]
+	# p = soup.find_all('p')
+	return ''.join(text)
 
 
 def clean_text(text):
-    chapters = {}
-    for chapter in text:
-        text_string = text[chapter]
-        sent_text = nltk.sent_tokenize(text_string)  # this gives us a list of sentences
-        # now loop over each sentence and tokenize it separately
-        tokenized_sent_text = []
-        #'''
-        for sentence in sent_text:
-            tokenized_text = nltk.word_tokenize(sentence)
-            tagged = nltk.pos_tag(tokenized_text)
-            tokenized_sent_text.append(tokenized_text)
-        #'''
-        #chapters[chapter] = tokenized_sent_text
-        chapters[chapter] = sent_text
-    return chapters
-
-
-
-
+	chapters = {}
+	for chapter in text:
+		text_string = text[chapter]
+		text_string = text_string.replace("\n", " ").strip()
+		sent_text = nltk.sent_tokenize(text_string)  # this gives us a list of sentences
+		chapters[chapter] = [sent_t for sent_t in sent_text if len(sent_t) > 0]
+	return chapters
